87a8be59c4623a737ffc42a42a1d0c4ab2c46503
Cosimo Giani <cosimogiani95@gmail.com>
2023-04-11 14:33:08 +0200

    Started implementing swing view

---
+++ museum/src/test/java/com/cosimogiani/museum/view/swing/SwingViewTest.java
@@ -0,0 +1,43 @@
+package com.cosimogiani.museum.view.swing;
+
+//import static org.junit.Assert.*;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(GUITestRunner.class)
+public class SwingViewTest extends AssertJSwingJUnitTestCase {
+	
+	private AutoCloseable closeable;
+	
+	private FrameFixture window;
+	
+	private SwingView swingView;
+	
+	@Override
+	protected void onSetUp() {
+		closeable = MockitoAnnotations.openMocks(this);
+		GuiActionRunner.execute(() -> {
+			swingView = new SwingView();
+			return swingView;
+		});
+		window = new FrameFixture(robot(), swingView);
+		window.show();
+	}
+	
+	@Override
+	protected void onTearDown() throws Exception {
+		closeable.close();
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
