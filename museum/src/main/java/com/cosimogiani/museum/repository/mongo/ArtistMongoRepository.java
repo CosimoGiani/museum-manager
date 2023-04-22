d31e46ea3834cbf7ebb2528833ae30450710efe5
Cosimo Giani <cosimogiani95@gmail.com>
2023-04-22 17:23:56 +0200

    Implemented artist repository and its unit tests

--- museum/src/main/java/com/cosimogiani/museum/repository/mongo/ArtistMongoRepository.java
+++ museum/src/main/java/com/cosimogiani/museum/repository/mongo/ArtistMongoRepository.java
@@ -1,14 +1,21 @@
 package com.cosimogiani.museum.repository.mongo;
 
-import org.bson.Document;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.cosimogiani.museum.model.Artist;
 import com.cosimogiani.museum.repository.ArtistRepository;
 import com.mongodb.MongoClient;
 import com.mongodb.client.ClientSession;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
-public class ArtistMongoRepository {
+public class ArtistMongoRepository implements ArtistRepository {
 	
 	private MongoCollection<Document> artistCollection;
 	private ClientSession session;
@@ -17,6 +24,40 @@ public class ArtistMongoRepository {
 		MongoDatabase db = client.getDatabase(dbName);
 		artistCollection = db.getCollection(artistCollectionName);
 		this.session = session;
+	}
+	
+	private Artist fromDocumentToArtist(Document d) {
+		return new Artist(d.get("_id").toString(), d.getString("name"));
+	}
+	
+	@Override
+	public List<Artist> findAllArtists() {
+		return StreamSupport
+				.stream(artistCollection.find(session).spliterator(), false)
+				.map(d -> fromDocumentToArtist(d))
+				.collect(Collectors.toList());
+	}
+	
+	@Override
+	public Artist findArtistById(String id) {
+		Document d = artistCollection.find(session, Filters.eq("_id", new ObjectId(id))).first();
+		if (d != null) {
+			return fromDocumentToArtist(d);
+		}
+		return null;
+	}
+	
+	@Override
+	public Artist saveArtist(Artist artist) {
+		Document newArtist = new Document().append("name", artist.getName());
+		artistCollection.insertOne(session, newArtist);
+		artist.setId(newArtist.get("_id").toString());
+		return artist;
+	}
+	
+	@Override
+	public void deleteArtist(String id) {
+		artistCollection.deleteOne(session, Filters.eq("_id", new ObjectId(id)));
 	}
 
 }
--- museum/src/test/java/com/cosimogiani/museum/repository/ArtistMongoRepositoryTest.java
+++ museum/src/test/java/com/cosimogiani/museum/repository/ArtistMongoRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.cosimogiani.museum.repository;
 
-import java.time.Duration;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -11,6 +16,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 
+import com.cosimogiani.museum.model.Artist;
 import com.cosimogiani.museum.repository.mongo.ArtistMongoRepository;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
@@ -37,7 +43,7 @@ public class ArtistMongoRepositoryTest {
 	
 	@Before
 	public void setup() {
-		client = new MongoClient(new ServerAddress(mongo.getContainerIpAddress(), mongo.getFirstMappedPort()));
+		client = new MongoClient(new ServerAddress(mongo.getContainerIpAddress(), mongo.getMappedPort(27017)));
 		MongoDatabase database = client.getDatabase(MUSEUM_DB_NAME);
 		database.drop();
 		database.createCollection(ARTIST_COLLECTION_NAME);
@@ -51,6 +57,65 @@ public class ArtistMongoRepositoryTest {
 	}
 	
 	@Test
-	public void test() {}
-
+	public void testFindAllArtistsWhenDatabaseIsEmpty() {
+		List<Artist> artists = artistRepository.findAllArtists();
+		assertThat(artists).isEmpty();
+	}
+	
+	@Test
+	public void testFindAllArtistsWhenDatabaseIsNotEmpty() {
+		Artist artist1 = new Artist("test1");
+		Artist artist2 = new Artist("test2");
+		addTestArtistToDatabase(artist1);
+		addTestArtistToDatabase(artist2);
+		List<Artist> artists = artistRepository.findAllArtists();
+		assertThat(artists).containsExactly(artist1, artist2);
+	}
+	
+	@Test
+	public void testFindArtistByIdNotFound() {
+		Artist artistFound = artistRepository.findArtistById("" + new ObjectId());
+		assertThat(artistFound).isNull();
+	}
+	
+	@Test
+	public void testFindArtistByIdFound() {
+		Artist artist = new Artist("test1");
+		String idArtist = addTestArtistToDatabase(artist);
+		addTestArtistToDatabase(new Artist("test2"));
+		Artist artistFound = artistRepository.findArtistById(idArtist);
+		assertThat(artistFound).isEqualTo(artist);
+	}
+	
+	@Test
+	public void testSaveArtist() {
+		Artist artistToAdd = new Artist("test");
+		Artist artistAdded = artistRepository.saveArtist(artistToAdd);
+		assertThat(artistAdded).isEqualTo(artistToAdd);
+		assertThat(readAllArtistsFromDatabase()).containsExactly(artistToAdd);
+		
+	}
+	
+	@Test
+	public void testDeleteArtistWhenArtistExistsInDatabase() {
+		Artist artist = new Artist("test");
+		String idArtistToDelete = addTestArtistToDatabase(artist);
+		artistRepository.deleteArtist(idArtistToDelete);
+		assertThat(readAllArtistsFromDatabase()).isEmpty();
+	}
+	
+	private String addTestArtistToDatabase(Artist artistToAdd) {
+		Document artist = new Document().append("name", artistToAdd.getName());
+		artistCollection.insertOne(artist);
+		artistToAdd.setId(artist.get("_id").toString());
+		return artist.get("_id").toString();
+	}
+	
+	private List<Artist> readAllArtistsFromDatabase() {
+		return StreamSupport
+				.stream(artistCollection.find().spliterator(), false)
+				.map(d -> new Artist(d.get("_id").toString(), d.getString("name")))
+				.collect(Collectors.toList());
+	}
+	
 }
